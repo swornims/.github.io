7ecc525d5890aee2921b7d78a4e4bf81e305d197
swornims <iamswornimshakya@gmail.com>
2022-07-01 13:53:22 +0545

    Added homepage jsx and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { Homepage } from './components/Homepage'
 
 function App() {
   return (
-    <h1>meow</h1>
+    <>
+      <Homepage />
+    </>
   );
 }
 
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Homepage.css'
+
+export const Homepage = () => {
+    return (
+        <div className='main-section'>
+            <h1>Hi.</h1>
+        </div>
+    )
+}
