3886824a8967bf6fd47ed74c03446e41f4a28fb7
swornims <iamswornimshakya@gmail.com>
2022-07-02 19:31:45 +0545

    Additional changes to about css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,7 @@ export const About = () => {
         <div className='about-container'>
             <section className='about-info'>
                 <h1 className='about-h1'>About Me</h1>
+                <hr className='about-hr' />
                 <p className='about-p'>Hello, I am Swornim. Based in Nepal. I am constantly increasing my
                     internal library of knowledge of computer languages.
                     My primarily work with Frontend but have experience working with Backend as well. </p>
