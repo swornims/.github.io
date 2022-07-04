22ec0e6a0e656f849e3318bde02e1e6b11c433a4
swornims <iamswornimshakya@gmail.com>
2022-07-04 12:33:12 +0545

    Added animations

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './About.css'
 import about from '../assets/about.svg'
 import github from '../assets/brands/github.png'
 import twitter from '../assets/brands/twitter.png'
 import linkedin from '../assets/brands/linkedin.png'
+import Aos from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 export const About = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, []);
+
     return (
         <div className='about-container'>
             <section className='about-info'>
                 <h1 className='about-h1'>About Me</h1>
-                <hr className='about-hr' />
-                <p className='about-p'>Hello, I am Swornim. Based in Nepal. I am constantly increasing my
-                    internal library of knowledge of computer languages.
-                    My primarily work with Frontend but have experience working with Backend as well. </p>
+                <hr data-aos="slide-right" className='about-hr' />
+                <p className='about-p'>Hello, I am Swornim. Based in Nepal. I have studied computer languages for a very long time now.
+                    I primarily work with Frontend and UI/UX Design but have also gained experience working with Backend by doing some local projects of my own. </p>
                 <span className='about-span'>Connect with Me</span>
 
                 <ul>
